885825938ba6f0f52211d5800c5c397e45cb6097
Babajide Ajibare <ajibarebabajide1@gmail.com>
2024-05-31 18:53:03 +0100

    feat: we worked on the header, naming it Hero

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import Hero from './Hero'
 import './App.css';
+import './Incubator-Logo 1.png'
+import './Image.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Hero/>
   );
 }
 
 export default App;
+
+// function FontAwesomeIcon(){
+//   return(
+//     <div>
+//     <FontAwesomeIcon icon={faHamburger} />
+//   </div>
+//   )
+// }
+
+// export default FontAwesomeIcon;
